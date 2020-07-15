72effbaaf45f20513444f8f33fe564de625c1bcf
jota <jorge-alberto.tolentino-garcia@gft.com>
2020-07-15 13:37:21 -0500

    Adding 01-hola-mundo branch

--- src/App.js
+++ src/App.js
@@ -7,18 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Bienvenidos a este curso</h2>
       </header>
+      <h3>Vamos a aprender React</h3>
+      <p>Hola mundo, estoy aprendiendo React</p>
+      <strong>Hola mundo con JSX</strong>
     </div>
   );
 }
