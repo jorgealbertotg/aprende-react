db0a384ab7576c92024dd6e811f94e14240ec243
jota <jorge-alberto.tolentino-garcia@gft.com>
2020-07-15 16:02:19 -0500

    Adding component state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+class Contador extends Component {
+  /*
+  Inicializacion del state dentro del constructor
+
+  constructor() {
+    super();
+    this.state = { contador: 1 };
+  }
+  */
+
+  /* Inicializacion del state mediante Class Fields*/
+  state = { contador: this.props.contadorInicial };
+
+  constructor(props) {
+    super(props);
+    setInterval(() => {
+      this.setState({ contador: this.state.contador + 1 });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>
+          Contador: <ContadorNumero numero={this.state.contador} />
+        </p>
+      </div>
+    );
+  }
+}
+
+Contador.defaultProps = {
+  contadorInicial : 0
+}
+
+class ContadorNumero extends Component {
+  render() {
+    return (
+      <span>{this.props.numero}</span>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Contador contadorInicial={100}/>
       </header>
     </div>
   );
