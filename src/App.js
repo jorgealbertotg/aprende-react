9ec4dc5f6e718188411d5285d71df88a5f682405
jota <jorge-alberto.tolentino-garcia@gft.com>
2020-07-15 18:08:57 -0500

    Adding an object list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import albums from './data/albums.json';
+
+class AlbumItem extends Component {
+  render() {
+    const { id, album } = this.props;
+    return (
+      <li>
+        <p>{id}</p>
+        <p><strong>Nombre:</strong> {album.name}</p>
+        <p><strong>Artista:</strong> {album.artist}</p>
+      </li>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <p>Albums</p>
+          <ul>
+            {
+              albums.map(album => {
+                return <AlbumItem key={album.id} id={album.id} album={album}/>
+              })
+            }
+          </ul>
+        </div>
       </header>
     </div>
   );
