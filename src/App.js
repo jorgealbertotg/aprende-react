37978d54aa343f6ec23f7913d0bce229d05ee7d1
jota <jorge-alberto.tolentino-garcia@gft.com>
2020-07-15 14:57:52 -0500

    Adding props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+class Body extends Component {
+  render() {
+    const {
+      encabezado,
+      nombre,
+      curso
+    } = this.props;
+    return (
+      <main>
+        <h1>{encabezado}</h1>
+        <p>
+          Bienvenido {nombre}
+        </p>
+        <p>
+          Este es el curso de {curso.nombre} de {curso.autor}
+        </p>
+      </main>
+    );
+  }
+}
+
+Body.defaultProps = {
+  encabezado : '¡Bienvenido! :)'
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Body nombre="Jorge" curso={{autor: 'Udemy', nombre: "React"}}/>
     </div>
   );
 }
