0f6bf5420af6c8f56df01f52597c42b419215a78
jota <jorge-alberto.tolentino-garcia@gft.com>
2020-07-16 09:35:35 -0500

    Using props children

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+class BoxDate extends Component {
+  render() {
+    return (
+      <div
+        style={{border: "1px solid #fff", padding: 10, margin: 10}}>
+          {this.props.children}
+      </div>
+    );
+  }
+}
+
+class Article extends Component {
+  render() {
+    const { title, author, date, children } = this.props;
+    return (
+      <article>
+        <h3>{title}</h3>
+        <p><em>Escrito por: {author}</em></p>
+        <BoxDate>{date}</BoxDate>
+        <div>
+          {children}
+        </div>
+      </article>
+    );
+  }
+}
 
 function App() {
   return (
@@ -8,16 +35,21 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Children props
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BoxDate>
+          Layout BoxDate 1
+        </BoxDate>
+        <BoxDate>
+          Layout BoxDate 2
+        </BoxDate>
+        <Article
+          title="Articulo sobre la prop children"
+          author="Tolentino"
+          date={new Date().toLocaleDateString()}>
+          <p>El contenido que envolvemos dentro del componente Article. Article sera enviado al componente como this.props.children</p>
+          <p><strong>Y mantiene las etiquetas y componentes que hayas añadido dentro</strong></p>
+        </Article>
       </header>
     </div>
   );
