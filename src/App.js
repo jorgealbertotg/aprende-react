60193f7e9805f7c2a58c00d618baae86fdb9d856
jota <jorge-alberto.tolentino-garcia@gft.com>
2020-07-15 20:48:59 -0500

    Adding form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputName: '',
+      inputTwitter: '@',
+      inputTerms: true
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  }
+  
+  handleChange = e => {
+    this.setState({ inputTerms: e.target.checked });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <h4>Formularios</h4>
+          <form onSubmit={this.handleSubmit}>
+            <p>
+              <label htmlFor="name">Nombre: </label>
+              <input id="name"
+                placeholder="Introduce tu nombre"
+                onChange={e => this.setState({ inputName: e.target.value })}
+                value={this.state.inputName} />
+            </p>
+            <p>
+              <label htmlFor="twitter">Twitter: </label>
+              <input id="twitter"
+                placeholder="Introduce tu Twitter"
+                onChange={e => this.setState({ inputTwitter: e.target.value })}
+                value={this.state.inputTwitter} />
+            </p>
+            <p>
+              <label htmlFor="terms">
+                <input id="terms"
+                  type="checkbox"
+                  onChange={this.handleChange}
+                  checked={this.state.inputTerms} />
+                 Acepto los terminos
+              </label>
+            </p>
+            <button>Enviar</button>
+          </form>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
