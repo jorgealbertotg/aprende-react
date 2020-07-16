d08573f2f1776c8e6aabccd287cf08ba18684bfc
jota <jorge-alberto.tolentino-garcia@gft.com>
2020-07-15 19:08:19 -0500

    Adding mouse move event

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { mouseX:0, mouseY: 0 };
+  }
+
+  handleMouseMove = e => {
+    const { clientX, clientY } = e
+    this.setState({ mouseX: clientX, mouseY: clientY });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div onMouseMove={this.handleMouseMove}
+            style={{border: '1px solid red', margin: 20, padding: 20}}>
+            <p>{this.state.mouseX}, {this.state.mouseY}</p>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
